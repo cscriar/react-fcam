80137e9529ee0cabbee4f67d03309ead7562cc83
cscriar <cscriar@gmail.com>
2022-03-03 01:54:45 -0300

    Final project - modulo cocluido

--- fcam/src/App.js
+++ fcam/src/App.js
@@ -9,9 +9,21 @@ class App extends Component {
      return (
        <Container component="article" maxWidth="sm">
          <Typography variant="h4" component="h2" align="center"> Formulário de Cadastro </Typography>
-         <FormularioCadastro />
+         <FormularioCadastro onSubmit={onSubmitForm} validarCPF={validarCPF}/>
        </Container>
      );
+   }
+ }
+
+ function onSubmitForm(dados) {
+   console.log(dados);
+ }
+
+ function validarCPF(cpf) {
+   if(cpf.lenght != 11) {
+    return {valido:false, texto:"CPF inválido!"}
+   }else {
+     return {valido:true, texto:""}
    }
  }
  
--- fcam/src/Components/FormularioCadastro/FormularioCadastro.jsx
+++ fcam/src/Components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,41 +2,63 @@ import React, {useState} from 'react';
 import {TextField, Button, Switch, FormControlLabel} from "@material-ui/core";
 
 
-function FormularioCadastro() {
+function FormularioCadastro({onSubmit, validarCPF}) {
     
    const [nome, setNome] = useState("");
    const [sobrenome, setSobrenome] = useState("");
    const [cpf, setCpf] = useState("");
    const [promocoes, setPromocoes] = useState(true);
    const [novidades, setNovidades] = useState(true);   
-   
+   const [erros, setErros] = useState({cpf:{valido:true, texto:""}})
    
     return(
       <form onSubmit={(event) => {
          event.preventDefault();
-         console.log(nome, sobrenome);
+         onSubmit({nome, sobrenome, cpf, promocoes, novidades});
+         // console.log(nome, sobrenome, cpf, promocoes, novidades);
          }}>
             
          <TextField 
             value={nome}
             onChange={(event) => {
-               let tmpNome = event.target.value;
-               if (tmpNome.length > 3) {
-                  tmpNome = tmpNome.substr(0 ,3);
-               }
-               setNome(tmpNome);
+               setNome (event.target.value);
             }}
-         id="nome" label="Nome" variant="outlined" fullWidth margin="normal"/>
+            id="nome" label="Nome" variant="outlined" fullWidth margin="normal"/>
+         
          <TextField 
-          value={sobrenome}
-          onChange={(event) => {
-             setSobrenome(event.target.value);
-          }}
-         id="sobrenome" label="Sobrenome" variant="outlined" fullWidth margin="normal"/>
-         <TextField id="cpf" label="CPF" variant="outlined" fullWidth margin="normal"/>
+            value={sobrenome}
+            onChange={(event) => {
+               setSobrenome(event.target.value);
+            }}
+            id="sobrenome" label="Sobrenome" variant="outlined" fullWidth margin="normal"
+         />
+         
+         <TextField 
+            value={cpf}
+            onChange={(event) => {
+               setCpf (event.target.value);
+            }}
+            onBlur={(event) => {
+               const ehValido = validarCPF(cpf);
+               setErros({cpf:ehValido});
+            }}
+            error={!erros.cpf.valido} 
+            helperText={erros.cpf.texto} 
+            id="cpf" label="CPF" variant="outlined" fullWidth margin="normal"/>
 
-         <FormControlLabel label="Promoções" control={<Switch name="promocoes" defaultChecked color="primary"/>} />
-         <FormControlLabel label="Novidades" control={<Switch name="novidades" defaultChecked color="primary"/>} />
+         <FormControlLabel label="Promoções" control={<Switch 
+            checked={promocoes} 
+            onChange={(event) => {
+            setPromocoes(event.target.checked);
+         }} 
+         name="promocoes" defaultChecked={promocoes} color="primary"/>} />
+         
+         <FormControlLabel label="Novidades" control={<Switch
+            checked={novidades} 
+            onChange={(event) => {
+            setNovidades(event.target.checked);
+         }} 
+         name="novidades" defaultChecked={novidades} color="primary"/>} />
          
          <Button type="submit" variant="contained" color="primary"> Cadastrar </Button>
       </form> 
