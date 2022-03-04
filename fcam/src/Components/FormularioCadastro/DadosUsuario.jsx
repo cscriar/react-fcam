831abf490f44aca5a1e8e9768cfe7c0059c64011
cscriar <cscriar@gmail.com>
2022-03-04 20:53:21 -0300

    formulário de dados

---
+++ fcam/src/Components/FormularioCadastro/DadosUsuario.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { TextField, Button } from "@material-ui/core";
+
+function DadosUsuario() {
+    return (
+        <form>
+            <TextField id="email" label="email" type="email" variant="outlined" fullWidth margin="normal"/>
+            <TextField id="senha" label="senha" type="password" variant="outlined" fullWidth margin="normal"/>
+            <Button type="submit" variant="contained" color="primary"> Cadastrar </Button>
+        </form>
+    );
+}
+
+export default DadosUsuario;
